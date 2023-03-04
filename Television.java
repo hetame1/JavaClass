82b9ac686d791f84da79561b2476a8d300fccefd
hetame1 <121005861+hetame1@users.noreply.github.com>
2023-03-04 19:11:03 +0900

    1

--- Television.java
+++ Television.java
@@ -7,7 +7,8 @@ public class Television {
         System.out.println("채널은" + channel + "이고 볼륨은 " + volume + "입니다");
     }
 
+    // 채널 변수값 반환
     int getChannel() {
-        return channel;
+        return channel; 
     }
 }
--- TelevisionTest.java
+++ TelevisionTest.java
@@ -1,10 +1,13 @@
 public class TelevisionTest {
     public static void main(String[] args) {
-        Television myTv = new Television();
+
+        Television myTv = new Television(); // 객체 생성
+        
         myTv.channel = 7;
         myTv.volume = 9;
         myTv.onOff = true;
-        int ch = myTv.getChannel();
-        System.out.println("현재 채널은" + ch + "입니다");
+
+        int ch = myTv.getChannel(); // ch 에 Television클래스에 멤버 변수값 받기
+        System.out.println("현재 채널은" + ch + "입니다"); // 채널 출력
     }
 }
