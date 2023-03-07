c2326ada6a5cfa9c39a93058df56183fef5c4867
hetame1 <hetame@g.yju.ac.kr>
2023-03-07 20:32:32 +0900

    1

---
+++ Boxtest.java
@@ -0,0 +1,15 @@
+public class Boxtest {
+  public static void main(String[] args) {
+
+    Box myBox = new Box();
+
+    myBox.width = 20;
+    myBox.length = 20;
+    myBox.height = 30;
+    
+
+    System.out.println("상자의 가로, 세로, 높이는 " + myBox.width + "," + myBox.length + "," + myBox.height );
+
+    
+  }
+} 
